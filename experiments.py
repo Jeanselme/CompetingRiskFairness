dcc17e04b4ff4a5153329f63126560fcdd10f1a2
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2023-11-01 16:32:23 -0400

    Initial experiments

---
+++ experiments.py
@@ -0,0 +1,77 @@
+
+# Comparsion models for competing risks
+# In this script we train the different models for competing risks
+import sys
+from generate import *
+from experiment import *
+
+# Select random seed
+random_seed = int(sys.argv[1])
+
+print("Script running experiments on generated data with seed =", random_seed)
+x, t, e, _, _ = generate(random_seed) 
+
+# Normalise data
+x, t, e = StandardScaler().fit_transform(x.values).astype(float),\
+            t.values.astype(float),\
+            e.values.astype(int)
+
+# Hyperparameters
+max_epochs = 1000
+grid_search = 10
+layers = [[i] * (j + 1) for i in [25, 50] for j in range(4)]
+layers_large = [[i] * (j + 1) for i in [25, 50] for j in range(8)]
+batch = [1000]
+
+# DSM
+param_grid = {
+    'epochs': [max_epochs],
+    'learning_rate' : [1e-3, 1e-4],
+    'batch': batch,
+
+    'k' : [2, 3, 4, 5],
+    'distribution' : ['LogNormal', 'Weibull'],
+    'layers' : layers_large,
+}
+DSMExperiment.create(param_grid, k = 1, n_iter = grid_search, path = 'Results/generate={}_dsm'.format(random_seed), random_seed = random_seed, delete_log = True).train(x, t, e)
+DSMExperiment.create(param_grid, k = 1, n_iter = grid_search, path = 'Results/generate={}_dsmnc'.format(random_seed), random_seed = random_seed, delete_log = True).train(x, t, (e == 1).astype(int))
+
+# NFG Competing risk
+param_grid = {
+    'epochs': [max_epochs],
+    'learning_rate' : [1e-3, 1e-4],
+    'batch': batch,
+    
+    'dropout': [0., 0.25, 0.5, 0.75],
+
+    'layers_surv': layers,
+    'layers' : layers,
+}
+NFGExperiment.create(param_grid, k = 1, n_iter = grid_search, path = 'Results/generate={}_nfg'.format(random_seed), random_seed = random_seed, delete_log = True).train(x, t, e)
+NFGExperiment.create(param_grid, k = 1, n_iter = grid_search, path = 'Results/generate={}_nfgnc'.format(random_seed), random_seed = random_seed, delete_log = True).train(x, t, (e == 1).astype(int))
+NFGExperiment.create(param_grid, k = 1, n_iter = grid_search, path = 'Results/generate={}_nfgcs'.format(random_seed), random_seed = random_seed, delete_log = True).train(x, t, e, cause_specific = True)
+
+# Desurv
+param_grid = {
+    'epochs': [max_epochs],
+    'learning_rate' : [1e-3, 1e-4],
+    'batch': batch,
+
+    'layers_surv': layers_large,
+    'layers': layers_large,
+    'act': ['Tanh'],
+}
+DeSurvExperiment.create(param_grid, k = 1, n_iter = grid_search, path = 'Results/generate={}_ds'.format(random_seed), random_seed = random_seed, delete_log = True).train(x, t, e)
+DeSurvExperiment.create(param_grid, k = 1, n_iter = grid_search, path = 'Results/generate={}_dsnc'.format(random_seed), random_seed = random_seed, delete_log = True).train(x, t, (e == 1).astype(int))
+
+# DeepHit Competing risk
+param_grid = {
+    'epochs': [max_epochs],
+    'learning_rate' : [1e-3, 1e-4],
+    'batch': batch,
+
+    'nodes' : layers,
+    'shared' : layers
+}
+DeepHitExperiment.create(param_grid, k = 1, n_iter = grid_search, path = 'Results/generate={}_dh'.format(random_seed), random_seed = random_seed, delete_log = True).train(x, t, e)
+DeepHitExperiment.create(param_grid, k = 1, n_iter = grid_search, path = 'Results/generate={}_dhnc'.format(random_seed), random_seed = random_seed, delete_log = True).train(x, t, (e == 1).astype(int))
---
+++ generate.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import gompertz
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+
+def generate(random_seed = 42, size = 10000, competing_risks = 2):
+    """
+        Generate two clusters with different survival profiles
+        Concatenate clusters and covariates for X
+        Model two different outcomes
+
+        return x, t, e, betas, z
+    """
+    np.random.seed(random_seed)
+    # Data - Generate two blobs
+    x, z = make_blobs(n_samples = size, n_features = 2, centers = ([-1.5, -1.5], [1.5, 1.5]), random_state = random_seed)
+    x = np.column_stack([x] + [np.random.normal(size = size) for _ in range(10)]) 
+
+    betas = {event: np.array([np.random.normal(size = 12) for _ in np.unique(z)]) for event in range(competing_risks)}
+
+    # Outcome
+    outcomes = {
+        event: np.zeros(size) for event in range(competing_risks + 1)   
+    }
+
+    # Create the different event times
+    for event in range(competing_risks):
+        params = np.exp((betas[event][z] * x).sum(1))
+        # Ensure positivity parameters
+        outcomes[event + 1] = gompertz.rvs(params, random_state = random_seed)
+
+    # Create censoring
+    outcomes[0] = gompertz.rvs(np.exp(np.random.random(len(x))) / 5, random_state = random_seed)
+
+    outcomes = pd.DataFrame.from_dict(outcomes)
+    outcomes['duration'], outcomes['event'] = outcomes.min(axis = 1), outcomes.idxmin(axis = 1)
+    outcomes['cluster'] = z
+
+    return pd.concat([pd.DataFrame(x), pd.Series(z, name = 'z')], axis = 1), outcomes['duration'], outcomes['event'], betas, outcomes
+
+def compute_cif(x, betas, z, times):
+    cif = {}
+
+    for event in range(len(betas)):
+        # The cif in closed form is a compretz with c1+c2 parameter (c1 being the parameter for the current risk and c2 for the competing)
+        # Futher normalised by c1/ (c1+c2)
+        c1 = np.exp((betas[event][z] * x.drop(columns = 'z')).sum(1))
+        c2 = np.exp((betas[1 - event][z] * x.drop(columns = 'z')).sum(1))
+        cif[event + 1] = pd.DataFrame(np.vstack([c1_ / (c1_ + c2_) * gompertz.cdf(times, c1_ + c2_) for c1_, c2_ in zip(c1, c2)]), columns = times)
+
+    return pd.concat(cif, 1)
