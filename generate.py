ececbdccd4aedc8565d8c9008b6546c2be440980
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2023-11-13 15:02:25 -0500

    More complex generation

--- generate.py
+++ generate.py
@@ -3,6 +3,10 @@ import pandas as pd
 from scipy.stats import gompertz, bernoulli
 from sklearn.datasets import make_blobs
 
+shape1 = lambda p, x: ((p * x) ** 2).sum(1)
+shape2 = lambda p, x: np.exp((p * x).sum(1))
+scale =  lambda p, x: np.exp((p * x).sum(1))
+
 def generate(random_seed = 42, size = 10000):
     """
         Generate two clusters with different survival profiles
@@ -18,18 +22,17 @@ def generate(random_seed = 42, size = 10000):
 
     # Generate parameters for each gompretz cause specific hazards
     # parameters[0] is used for scale of the gompretz shared across risk
-    parameters = {event: np.array([np.random.normal(size = 12) for _ in np.unique(z)]) / np.linspace(5, 10, 12) for event in range(3)}
+    parameters = {event: np.array([np.random.normal(size = 12) for _ in np.unique(z)]) / 3 for event in range(3)}
     parameters[0] /= 5
 
     # Generate the data with the summed hazard
-    shape = np.exp((parameters[1][z] * x).sum(1)) + np.exp((parameters[2][z] * x).sum(1)) 
-    scale = np.exp((parameters[0][z] * x).sum(1))
-    outcomes = gompertz.rvs(shape, scale = scale, random_state = random_seed)
+    s1 = shape1(parameters[1][z], x)
+    s2 = shape2(parameters[2][z], x)
+    sc = scale(parameters[0][z], x)
+    outcomes = gompertz.rvs(s1 + s2, scale = sc, random_state = random_seed)
 
     # Assign the outcomes following Bernoulli draw
-    hazard_1 = np.exp((parameters[1][z] * x).sum(1))
-    hazard_2 = np.exp((parameters[2][z] * x).sum(1))
-    hazard_ratio = hazard_1 / (hazard_1 + hazard_2)
+    hazard_ratio = s1 / (s1 + s2)
     events = 2 - bernoulli.rvs(hazard_ratio)
 
     # Create censoring
@@ -45,16 +48,12 @@ def generate(random_seed = 42, size = 10000):
 def compute_cif(x, betas, z, times):
     # As we know each cause specific hazard, we can model the associated gompretz
     cif = {}
-
     x = x.drop(columns = 'z')
+    shape = {1: shape1(betas[1][z], x),
+             2: shape2(betas[2][z], x)}
+    sc = scale(betas[0][z], x)
     for event in [1, 2]:
-        shape = np.exp((betas[event][z] * x).sum(1))
-        scale = np.exp((betas[0][z] * x).sum(1))
-
-        hazard_1 = np.exp((betas[event][z] * x).sum(1))
-        hazard_2 = np.exp((betas[3 - event][z] * x).sum(1))
-        hazard_ratio = hazard_1 / (hazard_1 + hazard_2)
-
-        cif[event] = pd.DataFrame(np.vstack([r * gompertz.cdf(times, sh, scale = sc) for r, sh, sc in zip(hazard_ratio, shape, scale)]), columns = times)
+        hazard_ratio = shape[event] / (shape[1] + shape[2])
+        cif[event] = pd.DataFrame(np.vstack([r * gompertz.cdf(times, sh, scale = s) for r, sh, s in zip(hazard_ratio, shape[event], sc)]), columns = times)
 
     return pd.concat(cif, axis = 1)
